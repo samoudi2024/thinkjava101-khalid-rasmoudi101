5260aab2be79dbb432cbdec6fb5a009898b486b3
samoudi2024 <krasmoudi@student.qcc.edu>
2026-09-18 01:02:18 +0000

    Complete Week 01 Hello starter

--- student-work/_template/week01/src/Week01HelloStarter.java
+++ student-work/_template/week01/src/Week01HelloStarter.java
@@ -1,8 +1,16 @@
 public class Week01HelloStarter {
 
     public static void main(String[] args) {
-        System.out.println("TODO: replace this with your name.");
+       /*System.out.println("TODO: replace this with your name.");
         System.out.println("TODO: add your course goal.");
-        System.out.println("TODO: add one more line about yourself.");
+        System.out.println("TODO: add one more line about yourself.");*/
+
+
+
+        
+        System.out.println("TKhalid Rasmoudi");
+        System.out.println("Java Programming.");
+        System.out.println("I am excited to learn more about Java!");  
+     
     }
 }
